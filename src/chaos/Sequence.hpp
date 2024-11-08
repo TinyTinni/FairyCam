69e3a373761aff8adc51330004660cc00a44d325
Matthias Möller <m_moeller@live.de>
2024-11-08 18:29:57 +0100

    add gcc14 support to actions (#9)

--- src/chaos/Sequence.hpp
+++ src/chaos/Sequence.hpp
@@ -8,6 +8,9 @@ class Sequence
     virtual void checkIsOpen() = 0;
     virtual void checkRetrieve() = 0;
     virtual void checkGrab() = 0;
-    virtual ~Sequence() = default;
+    virtual ~Sequence();
 };
+
+// workaround for GCC14, othewise, it segfaults
+inline Sequence::~Sequence() = default;
 } // namespace FairyCam
